88d6a906c33bdcf838074fd54b9c29b3fab5ff51
Alon (PC) <downtown2u@gmail.com>
2020-06-29 11:18:38 +0300

    Now saves the selected language as a cookie

--- js/load_i18n.js
+++ js/load_i18n.js
@@ -26,6 +26,10 @@ function load_i18n(lang) {
         // Update the text of the language selector to show the selecter language
         $("#cur-lang").text(json_data[lang]["name"]);
     });
+
+    /* Save the selected language as a cookie,
+    to load automaticly next time the user visites the website. */
+    Cookies.set("language", lang, { expires: 100 });
 }
 
 function load_lang_selector() {
@@ -40,5 +44,21 @@ function load_lang_selector() {
     });
 }
 
+function load_cookie_lang() {
+    /* This function is called on page load.
+    it checks a cookie that specifies the language of the user exicts,
+    and if it does it pulled the saved value and loads the saved language.
+    if it's the first time the user visites the website (or the language cookie expired),
+    a propmpt will popup usking the lanugae of the user. */
+
+    lang_cookie = Cookies.get("language");
+
+    if (lang_cookie != null) {
+        load_i18n(lang_cookie);
+    } else {
+        load_i18n("en");
+    }
+}
+
 load_lang_selector("i18n/languages.json");
-load_i18n("en");
+load_cookie_lang();
