f9cdd63b811f9bdb94fc010f916f86ee11d6436f
Alon (PC) <downtown2u@gmail.com>
2020-07-04 12:51:22 +0300

    Added countdown and countup settings panel

--- js/select_timer_mode.js
+++ js/select_timer_mode.js
@@ -2,10 +2,14 @@
 and switches between them when the mode button are clicked. */
 
 function __config_select_countup() {
+    $(".timer-settings").hide();
+    $("#countup-settings").show();
     __config_select_mode();
 }
 
 function __config_select_countdown() {
+    $(".timer-settings").hide();
+    $("#countdown-settings").show();
     __config_select_mode();
 }
 
